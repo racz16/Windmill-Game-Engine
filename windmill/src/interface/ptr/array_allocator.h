436fb18fc78ac718e7ee9f3e703e092fd697195f
Rácz Zalán <racz1666@gmail.com>
2022-02-22 00:26:44 +0100

    linux fix

--- windmill/src/interface/ptr/array_allocator.h
+++ windmill/src/interface/ptr/array_allocator.h
@@ -18,7 +18,7 @@ namespace wm {
 		template<class T>
 		static ptr<T> get(const key<T> key, const int32_t index) {
 			WM_ASSERT(contains(key));
-			auto va = arrays.at(key.get_hash()).convert<vector_array<T>>();
+			auto va = arrays.at(key.get_hash()).template convert<vector_array<T>>();
 			return va->get(index);
 		}
 
@@ -34,21 +34,21 @@ namespace wm {
 			auto va = ptr<vector_array<T>>(new vector_array<T>(
 				[](const int32_t array_size) {return new CT[array_size]; },
 				[](T* array) { delete[] static_cast<CT*>(array); }
-			)).convert<vector_array<void>>();
+			)).template convert<vector_array<void>>();
 			arrays.insert_or_assign(key.get_hash(), va);
 		}
 
 		template<class T>
 		static ptr<T> create(const key<T> key) {
 			WM_ASSERT(contains(key));
-			auto va = arrays.at(key.get_hash()).convert<vector_array<T>>();
+			auto va = arrays.at(key.get_hash()).template convert<vector_array<T>>();
 			return va->create();
 		}
 
 		template<class T>
 		static void destroy(const key<T> key, const ptr_view<T> ptr) {
 			WM_ASSERT(contains(key));
-			arrays.at(key.get_hash()).convert<vector_array<T>>()->destroy(ptr);
+			arrays.at(key.get_hash()).template convert<vector_array<T>>()->destroy(ptr);
 		}
 
 		static void destroy();
